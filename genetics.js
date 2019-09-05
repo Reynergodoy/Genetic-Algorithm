21302392aad8bfd13f71c2180a9829e55f549ce7
Rey <reyneraccel@gmail.com>
2019-09-05 17:00:00 -0300

    Create genetics.js

---
+++ genetics.js
@@ -0,0 +1,31 @@
+class Individual {
+  constructor () {
+    this.genes = [];
+    this.target = [];
+    this.fitness = 0;
+    this.generation = 0;
+  }
+  draw (canvas) {
+    
+  }
+  mutate () {
+  }
+  compare () {
+    
+  }
+}
+
+class Genotypes {
+  constructor (points, color) {
+    this.points = points;
+    this.color = color;
+  }
+  mutate () {
+  }
+  move () {
+    
+  }
+  changeColor (index) {
+    
+  }
+}
