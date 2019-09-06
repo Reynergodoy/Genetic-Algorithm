6ad3398262bfae9d784afc677ef08024a6f6b516
Rey <reyneraccel@gmail.com>
2019-09-06 19:00:18 -0300

    Create train.js

---
+++ train.js
@@ -0,0 +1,30 @@
+const _canvas = document.createElement('canvas');
+const ctx = _canvas.getContext('2d');
+const hidden = document.getElementById('hidden');
+const img = document.createElement('img');
+const ind = new Individual(50, 0.5);
+hidden.appendChild(_canvas);
+
+function setup (src) {
+  ind.init(canvas);
+  img.addEventListener ('load', () => {
+    ctx.drawImage(img, 0, 0);
+    ind.target = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  });
+  img.src = src;
+}
+
+function train (iterations) {
+  const context = canvas.getContext('2d');
+  for (let i = 0; i < iterations; i++) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    ind.mutate();
+    ind.draw(context);
+    const newFitness = ind.compare(context);
+    if (newFitness > ind.fitness) {
+      ind.fitness = newFitness;
+    } else {
+      ind.rollback();
+    }
+  }
+}
